a685bcceca9eb3ee16d07e975a74fc35bfffa0ea
Ivan Volokhov <ivanvolohov12@gmail.com>
2025-03-05 18:41:22 +0300

    dockerfile

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import uvicorn
 from .routers import products, carts, users
 app = FastAPI()
 
 app.include_router(products.router)
 app.include_router(carts.router)
 app.include_router(users.router)
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", host="0.0.0.0")
